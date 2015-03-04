9ec99be8bb31b6fd4764a93a4b4bd1aaf46c027a
Dan Stoppelman <dstop75@gmail.com>
2015-03-04 14:27:58 -0500

    add button to change background to red

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
   $('#blue').on('click', function(){
     $('body').css('background-color', 'blue');
   })
+
+  $('#red').on('click', function(){
+    $('body').css('background-color', 'red');
+  })
 });
